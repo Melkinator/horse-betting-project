c9a224f9f9907a8b4c849b6b53f12c9584437217
Melkinator <aacrazykid@yahoo.com>
2026-02-09 14:08:30 +0700

    feat: week-3 work

--- week-3/Main.java
+++ week-3/Main.java
@@ -1,95 +1,8 @@
-class Horse {
-    int id;
-    String name;
-    String breed;
-    double weight;
-    boolean forSale;
-
-    Horse(int id, String name, String breed, double weight, boolean forSale) {
-        this.id = id;
-        this.name = name;
-        this.breed = breed;
-        this.weight = weight;
-        this.forSale = forSale;
-    }
-
-    void printHorse() {
-        System.out.println("Horse: " + name + ", Breed: " + breed + ", Weight: " + weight);
-    }
-}
-
-class Stable {
-    String stableName;
-    Horse[] horses;
-    int horseCount;
-    int capacity;
-
-    Stable(String stableName, int capacity) {
-        this.stableName = stableName;
-        this.capacity = capacity;
-        horses = new Horse[capacity];
-        horseCount = 0;
-    }
-
-    void addHorse(Horse h) {
-        if (horseCount < capacity) {
-            horses[horseCount] = h;
-            horseCount++;
-        }
-    }
-
-    Horse findHorseByName(String name) {
-        for (int i = 0; i < horseCount; i++) {
-            if (horses[i].name.equals(name)) {
-                return horses[i];
-            }
-        }
-        return null;
-    }
-}
-
-class Staff {
-    String staffName;
-    int staffId;
-    Horse assignedHorse;
-    Stable workplace;
-
-    Staff(String staffName, int staffId, Horse assignedHorse, Stable workplace) {
-        this.staffName = staffName;
-        this.staffId = staffId;
-        this.assignedHorse = assignedHorse;
-        this.workplace = workplace;
-    }
-
-    void printStaff() {
-        System.out.println(staffName + " cares for " + assignedHorse.name +
-                " at " + workplace.stableName);
-    }
-}
-
-class Buyer {
-    String buyerName;
-    Horse horse;
-    double snapshotWeight;
-    double budget;
-
-    Buyer(String buyerName, Horse horse, double budget) {
-        this.buyerName = buyerName;
-        this.horse = horse;
-        this.budget = budget;
-
-        // SNAPSHOT (primitive copy)
-        snapshotWeight = horse.weight;
-    }
-
-    void printBuyerInfo() {
-        System.out.println("Buyer: " + buyerName);
-        System.out.println("Horse weight at purchase time: " + snapshotWeight);
-    }
-}
-
-class Main {
+public class Main {
     public static void main(String[] args) {
+        // int i = 0;
+        // String s = "hi";
+        // String s1 = new String("je");
 
         Horse h1 = new Horse(1, "Still in Love", "Thoroughbred", 500, true);
         Horse h2 = new Horse(2, "Wonder Acute", "Thoroughbred", 480, false);
@@ -108,22 +21,22 @@ class Main {
         Staff staff = new Staff("Melk", 101, h1, stable);
         staff.printStaff();
 
-        // f1
+        // f1 primitive copy
         int original = 5;
         int copy = original;
         copy = 10;
         System.out.println("f1: original = " + original + ", copy = " + copy);
 
-        // f2
+        // f2 reference copy
         Horse refHorse = h1;
         refHorse.weight = 550;
         System.out.println("f2: h1.weight = " + h1.weight);
 
-        // f3
+        // f3 array store references
         h2.name = "Neo Universe";
         System.out.println("f3: horse in the array = " + stable.horses[1].name);
 
-        // f4
+        // f4 snapshot behavior
         Buyer buyer = new Buyer("John", h1, 10000);
         h1.weight = 600;
         buyer.printBuyerInfo();
